1f3f96400c5dcd448b00d83255dee3dd6c66a9c9
Thomassen <noveng3ance@gmail.com>
2019-02-13 13:34:06 +0100

    gulp project
    Gulp setup using live preview, less to css build, and image minifying

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp')
+const less = require('gulp-less')
+const browserSync = require('browser-sync').create()
+const imagemin = require('gulp-imagemin');
+
+gulp.task('less', function () {
+    gulp.src('less/*.less')
+        .pipe(less({
+            paths: [ 'less' ]
+        }))
+        .pipe(gulp.dest('css'))
+});
+
+gulp.task('browser-sync', function(){
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    })
+})
+
+gulp.task('imgMin', function(){
+    gulp.src('imgs/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('default', ['less', 'browser-sync', 'imgMin'], function(){
+    gulp.watch('less/*.less', ['less'])
+    gulp.watch("*.html").on('change', browserSync.reload)
+})
